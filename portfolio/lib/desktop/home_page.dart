dff504f1ab02edcaddcdc7061b0c76d0f2ce670d
SanthoshKumar-PS <santhoshkumarsakthi2003@gmail.com>
2025-06-24 15:59:40 +0530

    Changes Profile

--- portfolio/lib/desktop/home_page.dart
+++ portfolio/lib/desktop/home_page.dart
@@ -53,10 +53,7 @@ class HomePageImage extends StatelessWidget {
     return CircleAvatar(
       minRadius: MediaQuery.of(context).size.width*0.10,
       maxRadius: MediaQuery.of(context).size.width*0.10,
-      backgroundImage: NetworkImage('assets/images/profile.jpg'),
-      // backgroundImage: NetworkImage(
-      //   "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
-      // ),
+      backgroundImage: AssetImage('assets/images/profile.jpg'),
     );
   }
 }
